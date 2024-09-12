f40b14f77fa57150a2ea685fab7d651086b8a1bf
Ryan Custer <ryancuster43@gmail.com>
2024-09-11 19:30:16 -0600

    update

--- logic.js
+++ logic.js
@@ -1,9 +1,9 @@
-/*function calculateSum() {
+function calculateSum() {
     let x = 0;
     let total = 0;
     let level = parseInt(document.getElementById("level").value);
     let newLevel = parseInt(document.getElementById("newLevel").value);
-
+    let loopValue = newLevel - level;
     // Validate input
     if (isNaN(level) || isNaN(newLevel) || newLevel <= level) {
         document.getElementById('result').innerText = "Please enter valid levels.";
@@ -11,7 +11,7 @@
     }
 
     // Calculating the total runes required
-    for (let i = 0; i < (newLevel - level); i++) {
+    for (let i = 0; i < (loopValue); i++) {
         x = ((level + 81) - 92) * 0.02;
         if (x < 0) x = 0;
         let runeCost = Math.floor(((x + 0.1) * (((level) + 81))**2) + 1);
@@ -21,18 +21,4 @@
 
     // Display the result
     document.getElementById('result').innerText = `You need ${total} runes to get to level ${newLevel}.`;
-}*/
-let level = 1
-let newLevel = 500
-let x = 0;
-let total = 0;
-let count = 0;
-for (let i = 0; i < (newLevel - level); i++) {
-    count++;
-    x = ((level + 81) - 92) * 0.02;
-    if (x < 0) x = 0;
-    let runeCost = Math.floor(((x + 0.1) * (((level) + 81))**2) + 1);
-    level += 0;
-    total += runeCost;
 }
-console.log(total , count)
